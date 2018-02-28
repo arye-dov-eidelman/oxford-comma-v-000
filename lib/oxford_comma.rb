b39f5e91d62b5737f4369cd4e74ceda836878207
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-28 03:54:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
-def oxford_comma(array)
+def oxford_comma(true)
   case array.length
-  when 1
+  when array.length == 1
     array[0]
-  when 2
+  when array.length == 2
     array[0] and array[1]
   when (array.length >= 3)
     result = ''
