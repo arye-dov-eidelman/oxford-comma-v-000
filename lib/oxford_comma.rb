ecb2c55bcd9aebc39911590ac7c3013634059d39
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-02-28 03:50:17 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,12 @@
 def oxford_comma(array)
   case array.length
   when 1
-
-
+    array[0]
   when 2
-
-
+    array[0] and array[1]
   when (array.length >= 3)
-
+    array.each_and_index do |string, i|
+      puts 'and ' if i==array.length-1 + string + ', ' if i!=array.length-1}"
 
   end
 end
